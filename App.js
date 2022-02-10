dcfc18ce209c177e3249d76d50ad2056cb2f9b0f
Voffka Dubinkin <vl.dubinkin@gmail.com>
2022-02-10 22:48:30 +0100

    find cout vawels - first method

--- App.js
+++ App.js
@@ -161,3 +161,42 @@ let num = 0;
 
 //------------------------------------------------------------
 
+
+
+/* 
+
+Description:
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+*/
+
+function getCount(str) {
+  let vowelsCount = 0;
+  const vowels = "aeiou";
+
+  for(let char of str.toLowerCase()){
+    if(vowels.includes(char)){
+      vowelsCount += 1;
+    }
+  }
+  return vowelsCount;
+}
+
+// второй способ
+
+function getCount(str) {
+  var vowelsCount = 0;
+  str.split("").forEach(function(x){
+    if(x == "a" | x == "e" | x == "i" | x == "o" | x == "u"){
+      vowelsCount += 1;
+    }
+  });  
+  return vowelsCount;
+}
+
+//------------------------------------------------------------
